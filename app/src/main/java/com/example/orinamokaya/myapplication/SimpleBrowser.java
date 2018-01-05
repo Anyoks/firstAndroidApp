1f785e2c28324a50e7ec67af938946c2e72bb294
Denn Orina <anyoksdenn@gmail.com>
2018-01-05 17:05:55 +0300

    setting up webview

--- app/src/main/java/com/example/orinamokaya/myapplication/SimpleBrowser.java
+++ app/src/main/java/com/example/orinamokaya/myapplication/SimpleBrowser.java
@@ -3,6 +3,7 @@ package com.example.orinamokaya.myapplication;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.webkit.WebView;
 
 /**
  * Created by orinamokaya on 1/5/18.
@@ -13,5 +14,11 @@ public class SimpleBrowser extends Activity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.simplebrowser);
+
+        //setting up  the vars
+        WebView ourBrowser= (WebView) findViewById(R.id.wvBrowser);
+        ourBrowser.loadUrl("http://www.google.com"); //default page
     }
 }

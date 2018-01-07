865409146c2b0c74947013e2a8ef84ebea90ffe5
Denn Orina <anyoksdenn@gmail.com>
2018-01-07 13:55:04 +0300

    enabled Java script and hide keyboard after input

--- app/src/main/java/com/example/orinamokaya/myapplication/SimpleBrowser.java
+++ app/src/main/java/com/example/orinamokaya/myapplication/SimpleBrowser.java
@@ -1,9 +1,11 @@
 package com.example.orinamokaya.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
@@ -25,9 +27,21 @@ public class SimpleBrowser extends Activity implements View.OnClickListener {
         setContentView(R.layout.simplebrowser);
 
 
-        ourBrowser= (WebView) findViewById(R.id.wvBrowser);
+        ourBrowser = (WebView) findViewById(R.id.wvBrowser);
+
+        //setting browwser settings
+        ourBrowser.getSettings().setJavaScriptEnabled(true); // enable javascript
+        ourBrowser.getSettings().setLoadWithOverviewMode(true); //zoom out of the page so you can see the whole page
+        ourBrowser.getSettings().setUseWideViewPort(true); //desktop version wesite
+
         ourBrowser.setWebViewClient(new WebViewClient()); // if you don't set this, the url will be opened in a browser
-        ourBrowser.loadUrl("http://www.miraclemanner.org"); //default page
+
+        //just incase the url does not load... catch the error
+        try {
+            ourBrowser.loadUrl("http://www.miraclemanner.org"); //default page
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         //setting up  the vars
         // setting up references to view
@@ -57,6 +71,10 @@ public class SimpleBrowser extends Activity implements View.OnClickListener {
                 String theLink = url.getText().toString();
                 ourBrowser.loadUrl(theLink);
 
+                //hide keyboard after clicking go
+                // first set up an input method manager
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(url.getWindowToken(),0); // this hides the keyboard
                 break;
             case R.id.bBack:
                 // if there's no go back link...we must check

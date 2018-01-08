aa59240c6d54e87546e6506f49497305a09769b2
Denn Orina <anyoksdenn@gmail.com>
2018-01-08 15:21:19 +0300

    set up spinner adapter

--- app/src/main/java/com/example/orinamokaya/myapplication/ExternalData.java
+++ app/src/main/java/com/example/orinamokaya/myapplication/ExternalData.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.Nullable;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 /**
@@ -22,6 +24,9 @@ public class ExternalData extends Activity {
     // these can only be accessed in this class
     private TextView canWrite, canRead;
     private String status;
+    boolean canR, canW;
+    Spinner spinner;
+    String[] paths = {"Music", "Pictures", "Download"}; // where we will store data
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,14 +44,31 @@ public class ExternalData extends Activity {
             //then we can read or write. you don't want to read or write to a storage that is not there.
             canRead.setText("true");
             canWrite.setText("true");
+            canR = canW = true;
+
         }else if (status.equals(Environment.MEDIA_MOUNTED_READ_ONLY)){
             //can read but can't write
             canRead.setText("true");
             canWrite.setText("false");
+
+            canR = true;
+            canW = false;
         }else{
             // we cannot read or write
             canRead.setText("false");
             canWrite.setText("false");
+
+            canR = canW = false;
         }
+
+        //the spinner accepts an array adapter in its params
+        // the array adapter data type depends on the data type you want. we are passing Strings so we use string
+        //fo the array adapter accepts the current class context and a spinner layout i.e simple spinner
+        //and the string array paths
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ExternalData.this, android.R.layout.simple_spinner_item, paths);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+
+
     }
 }

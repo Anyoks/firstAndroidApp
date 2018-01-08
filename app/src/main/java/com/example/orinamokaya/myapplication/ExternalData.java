316d94e7d9213e5952e0770512dac19cd241423f
Denn Orina <anyoksdenn@gmail.com>
2018-01-08 13:19:32 +0300

    added progress bar using progress dialio

--- app/src/main/java/com/example/orinamokaya/myapplication/ExternalData.java
+++ app/src/main/java/com/example/orinamokaya/myapplication/ExternalData.java
@@ -1,6 +1,7 @@
 package com.example.orinamokaya.myapplication;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -9,6 +10,7 @@ import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.io.File;
@@ -113,11 +115,22 @@ public class ExternalData extends Activity implements View.OnClickListener{
         // 1. onPreExecute 2. doInBackground 3. onProgressUpdate 4. onPostExecute
         //they are called in that order.
 
+        //setting up var for progess bar display
+//        ProgressBar progressBar;
+        ProgressDialog progressBar1;
+
         //this method will be exectured first in this Asynch task
-        protected void onPreExecute(String f){
-            //set up variables
-            f ="set up the varibale here";
-            //do some stuff
+        protected void onPreExecute(){
+            //set up the progress bar
+//            progressBar = new ProgressBar(ExternalData.this,null, android.R.attr.progressBarStyleHorizontal); // takes the context of the main class and progress bar styele
+            progressBar1 = new ProgressDialog(ExternalData.this);
+            progressBar1.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            progressBar1.setMax(100); //we could take the size of the bytes here. byte this is just an simple example
+//            progressBar.setVisibility(View.VISIBLE); //show progress bar
+//            progressBar.setVisibility(View.GONE); // disappear when done
+            progressBar1.setTitle("Loading data...");
+            progressBar1.show();//show
+
         }
 
         //this method will be executed second
@@ -127,6 +140,23 @@ public class ExternalData extends Activity implements View.OnClickListener{
             // FileInputStream is used to load data or read data
             String collectedData = null;
             FileInputStream fis = null;
+
+            //for progress bar movement. a simple example
+            for (int i = 0; i<100; i++ ){
+                //refer to the onProgressUpdate method
+                publishProgress(1); //this method calls the onPrigressUpdate method and passes in by 5 as the increment value
+//                it'llincrement really fast so to see it lets add a sleep
+                try {
+                    Thread.sleep(88);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            progressBar1.dismiss();
+
+
+
             try {
                 fis = openFileInput(FILENAME); //open the file via input stream
                 byte[] dataArray = new byte[fis.available()]; //get the length of the file
@@ -152,13 +182,15 @@ public class ExternalData extends Activity implements View.OnClickListener{
 
         //this is called third. Integer...ptogress is an integer array called progress
         protected void onProgressUpdate(Integer...progress){
-
+            //increment progress bar
+//            progressBar.incrementProgressBy(progress[0]); //increment by 5 which is at position 0 of the array
+            progressBar1.incrementProgressBy(progress[0]); //increment by 5 which is at position 0 of the array
         }
 
         //this method will be executed last after everything is completed. here is where we retrieve results
         protected void onPostExecute(String result){ // because we are returning a string called collected, we pass in a string var
             //this is where we set up or text view
-            String s = " was Loaded from a file in the internal storage";
+            String s = " --from internal storage";
             dataResults.setText(result + s);
         }
 

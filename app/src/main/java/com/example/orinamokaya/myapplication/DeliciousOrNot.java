caac619c42bb6ba5c72d21189b0c4e3e46457f4f
Denn Orina <anyoksdenn@gmail.com>
2018-01-15 16:06:10 +0300

    set up SQLITE database variables

--- app/src/main/java/com/example/orinamokaya/myapplication/DeliciousOrNot.java
+++ app/src/main/java/com/example/orinamokaya/myapplication/DeliciousOrNot.java
@@ -8,4 +8,20 @@ package com.example.orinamokaya.myapplication;
  */
 
 public class DeliciousOrNot {
+
+    // THESE ARE THE TABLE ROWS
+
+    //public data
+
+    public static final String KEY_ROWID = "_id"; // this will hold the row id of data entered into the database
+    public static final String KEY_NAME = "food_name"; // this will hold the name of data entered into the database
+    public static final String KEY_Delicacy = "food_delicacy"; // this will hold the delicacy of the food entered into the database
+
+
+    // Setting up the database
+    // private data
+    private static final String DATABASE_NAME = "DeliciousOrNot"; // databse name
+    private static final String DATABASE_TABLE = "foodTable"; // table name. it'll contain the rows above defined
+    private static final int DATABASE_VERSION = 1; //database version
+
 }
